936d619bc6df2f491547fb5bc3b141f248ca649d
Threated <jan2001.07@gmail.com>
2022-12-01 18:17:43 +0100

    Add cli lib

--- cmd/goget/main.go
+++ cmd/goget/main.go
@@ -1,32 +1,64 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/Threated/goget/pkg/utils"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        panic("Takes at least a Url")
-    }
-    info, err := utils.NewRepoInfoFromUrl(os.Args[len(os.Args) - 1])
-    if err != nil {
-        panic(err.Error())
-    }
-    println(info.String())
-    results := make(chan utils.Result)
-    go utils.Download(info, results)
-    for result := range results {
-        if result.Err != nil {
-            println(result.Err.Error())
-            if result.Context != nil {
-                println(result.Context.String())
-            }
-        } else {
-            // println("Downloaded: " + result.Context.Name)
-        }
-    }
+	app := &cli.App{
+		Name:           "goget",
+		Usage:          "Download a git subfolder or file",
+		DefaultCommand: "help",
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				TakesFile:   false,
+				Name:        "outputDir",
+				Aliases:     []string{"o"},
+				Value:       ".",
+				Usage:       "Dir to download into",
+				DefaultText: ".",
+			},
+			&cli.IntFlag{
+				Name:        "depth",
+				Aliases:     []string{"d"},
+				Usage:       "Number of recursions to download subfolders",
+				DefaultText: "all subfolders",
+			},
+			&cli.BoolFlag{
+				Name:    "verbose",
+				Aliases: []string{"v"},
+				Usage:   "Print names of downloaded files",
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			info, err := utils.NewRepoInfoFromUrl(cCtx.Args().First())
+			if err != nil {
+				return err
+			}
+			results := make(chan utils.Result)
+			go utils.Download(info, results)
 
+			// awaiting results from channel and loging them
+			for result := range results {
+				if result.Err != nil {
+					if result.Context != nil {
+						println("Error downloading " + result.Context.String())
+					}
+					return result.Err
+				}
+				if result.Context != nil && cCtx.Bool("verbose") {
+					println("Downloaded " + result.Context.String())
+				}
+			}
+			return nil
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 
 }
